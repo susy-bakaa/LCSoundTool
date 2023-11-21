87ffe49536b1092ca2c03ff37a88fe04b911595d
no00ob <aksu.kotikone@gmail.com>
2023-11-21 04:05:06 +0200

    Initial commit

---
+++ Patches/AudioSourcePatch.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using HarmonyLib;
+using UnityEngine;
+
+namespace LCSoundDebug.Patches
+{
+    [HarmonyPatch(typeof(AudioSource))]
+    internal class AudioSourcePatch
+    {
+        [HarmonyPatch(nameof(AudioSource.Play), new Type[] { })]
+        [HarmonyPrefix]
+        public static void LogPlayingAudio(AudioSource __instance)
+        {
+            if (Plugin.debugAudioSources)
+                Plugin.Instance.logger.LogDebug($"{__instance} at {__instance.transform.root} is playing {__instance.clip.name}");
+        }
+        [HarmonyPatch(nameof(AudioSource.Play), new[] { typeof(ulong) })]
+        [HarmonyPrefix]
+        public static void LogPlayingAudio2(AudioSource __instance)
+        {
+            if (Plugin.debugAudioSources)
+                Plugin.Instance.logger.LogDebug($"{__instance} at {__instance.transform.root} is playing {__instance.clip.name}");
+        }
+        [HarmonyPatch(nameof(AudioSource.Play), new[] { typeof(double) })]
+        [HarmonyPrefix]
+        public static void LogPlayingAudio3(AudioSource __instance)
+        {
+            if (Plugin.debugAudioSources)
+                Plugin.Instance.logger.LogDebug($"{__instance} at {__instance.transform.root} is playing {__instance.clip.name}");
+        }
+        [HarmonyPatch(nameof(AudioSource.PlayDelayed), new[] { typeof(float) })]
+        [HarmonyPrefix]
+        public static void LogPlayingDelayedAudio(AudioSource __instance)
+        {
+            if (Plugin.debugAudioSources)
+                Plugin.Instance.logger.LogDebug($"{__instance} at {__instance.transform.root}  is playing  {__instance.clip.name}");
+        }
+        [HarmonyPatch(nameof(AudioSource.PlayClipAtPoint), new[] { typeof(AudioClip), typeof(Vector3), typeof(float) })]
+        public static void LogPlayingClipAtPointAudio2(AudioSource ___audioSource)
+        {
+            if (Plugin.debugAudioSources)
+                Plugin.Instance.logger.LogDebug($"{___audioSource} at {___audioSource.transform.root} is playing {___audioSource.clip.name} at point");
+        }
+        [HarmonyPatch(nameof(AudioSource.PlayOneShotHelper))]
+        [HarmonyPrefix]
+        public static void LogOneShotPlayingAudio2(AudioSource source, AudioClip clip)
+        {
+            if (Plugin.debugAudioSources)
+            {
+                Plugin.Instance.logger.LogDebug($"{source} at {source.transform.root} is playing one shot {clip.name}");
+            }
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,63 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using GameNetcodeStuff;
+using HarmonyLib;
+using LCSoundDebug.Patches;
+using UnityEngine;
+
+namespace LCSoundDebug
+{
+    [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private const string PLUGIN_GUID = "LCSoundDebug";
+        private const string PLUGIN_NAME = "LC Sound Debugger";
+        private const string PLUGIN_VERSION = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(PLUGIN_GUID);
+
+        public static Plugin Instance;
+
+        internal ManualLogSource logger;
+
+        public static KeyboardShortcut toggleAudioSourceDebugLog;
+        public static bool wasKeyDown;
+
+        public static bool debugAudioSources;
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            logger = BepInEx.Logging.Logger.CreateLogSource(PLUGIN_GUID);
+
+            logger.LogInfo($"Plugin {PLUGIN_GUID} is loaded!");
+
+            toggleAudioSourceDebugLog = new KeyboardShortcut(KeyCode.F5, new KeyCode[0]);
+
+            debugAudioSources = false;
+
+            harmony.PatchAll(typeof(Plugin));
+			harmony.PatchAll(typeof(AudioSourcePatch));
+        }
+
+        [HarmonyPatch(typeof(PlayerControllerB), "Update")]
+        [HarmonyPrefix]
+        public static void HookPlayerControllerBUpdate()
+        {
+            if (toggleAudioSourceDebugLog.IsDown() && !wasKeyDown)
+            {
+                wasKeyDown = true;
+            }
+            if (toggleAudioSourceDebugLog.IsUp() && wasKeyDown)
+            {
+                wasKeyDown = false;
+                debugAudioSources = !debugAudioSources;
+                Instance.logger.LogDebug($"Toggling AudioSource debug logs {debugAudioSources}!");            }
+        }
+    }
+}
