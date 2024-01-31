f2c66c4bd3ed9b0e29d7da08024cbd5360c65d66
no00ob <aksu.kotikone@gmail.com>
2024-01-31 22:53:02 +0200

    cleanup

--- NetworkHandler.cs
+++ NetworkHandler.cs
@@ -138,44 +138,4 @@ namespace LCSoundTool.Networking
             SoundTool.Instance.logger.LogDebug($"Sending a new Unity random seed of {seed} to all clients...");
         }
     }
-
-    // Borrowed from https://forum.unity.com/threads/how-to-send-a-string-array-in-an-rpc.1466021/
-    public struct Strings : INetworkSerializable
-    {
-        string[] myStrings;
-
-        public Strings(string[] myStrings)
-        {
-            this.myStrings = myStrings;
-        }
-
-        public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
-        {
-            if (serializer.IsWriter)
-            {
-                FastBufferWriter fastBufferWriter = serializer.GetFastBufferWriter();
-                fastBufferWriter.WriteValueSafe(myStrings.Length);
-
-                for (int i = 0; i < myStrings.Length; i++)
-                {
-                    fastBufferWriter.WriteValueSafe(myStrings[i]);
-                }
-            }
-
-            if (serializer.IsReader)
-            {
-                FastBufferReader fastBufferReader = serializer.GetFastBufferReader();
-                fastBufferReader.ReadValueSafe(out int length);
-
-                myStrings = new string[length];
-
-                for (int i = 0; i < length; i++)
-                {
-                    fastBufferReader.ReadValueSafe(out myStrings[i]);
-                }
-            }
-        }
-
-        public string[] MyStrings { get => myStrings; set => myStrings = value; }
-    }
 }
